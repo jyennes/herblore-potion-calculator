8d9ccc82a3e5c8357a2c9cb7b7a6a91a47393042
jyennes <jyennes5@gmail.com>
2022-02-26 14:42:40 -0500

    loops for every potion, calculates profit

--- HerbloreCalc.py
+++ HerbloreCalc.py
@@ -14,9 +14,10 @@ import json
 api = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="
 
 # lists for each potion and their id's
-#TODO put potion lists in another list 
 attack = [121, 91, 221]
 poison = [175, 93, 235]
+# lists of potions
+potions = [attack, poison]
  
 # create dataframe and append potion rows to it
 def df_func(pot_id, base_id, ingr_id):
@@ -34,15 +35,18 @@ def df_func(pot_id, base_id, ingr_id):
     pot_price = pot_data['item']['current']['price']
     base_price = base_data['item']['current']['price']
     ingr_price = ingr_data['item']['current']['price']
+    pot_profit = pot_price - (base_price + ingr_price)
 
     # remove comma
     pot_price = str(pot_price).replace(',', '')
     base_price = str(base_price).replace(',', '')
     ingr_price = str(ingr_price).replace(',', '')
+    pot_profit = str(pot_profit).replace(',', '')
+
 
     pot_name = pot_data['item']['name']
-    base_name = base_data['item']['name']
-    ingr_name = ingr_data['item']['name']
+    # base_name = base_data['item']['name']
+    # ingr_name = ingr_data['item']['name']
 
     # Dataframe
     df = pd.DataFrame()
@@ -54,20 +58,24 @@ def df_func(pot_id, base_id, ingr_id):
     p_price_list = [pot_price]
     b_price_list = [base_price]
     i_price_list = [ingr_price]
+    profit_list = [pot_profit]
 
     # Assigning lists to named columns
     df['Potion'] = pot_list
     df['Pot price'] = p_price_list
     df['Base price'] = b_price_list
     df['Ingr Price'] = i_price_list
+    df['Profit/Pot'] = profit_list
 
     #TODO append to existing table
-    #TODO add column for profit
 
-    df
+    print(df)
 
 # Test
-df_func(*attack)
+list_length = len(potions)
+for x in range(list_length):
+    df_func(*potions[x])
 
-#TODO while loop using list of lists
+
 #TODO table data based on user entry for # of potions
+#TODO append to existing table instead of creating a new one.
